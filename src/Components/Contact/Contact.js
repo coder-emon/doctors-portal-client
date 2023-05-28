80020168d849df9c8cc5593c843e63d06ed5313f
coder-emon <developeremon.me@gmail.com>
2023-05-28 20:09:29 +0600

    contact form some modification

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -10,9 +10,9 @@ const Contact = () => {
             </div>
             <div>
                 <form className='flex flex-col items-center justify-center gap-5 '>
-                    <input type="email" placeholder='Email Address' className='bg-white rounded px-2 py-1 w-96 focus:ring-primary focus:ring-2 ' />
-                    <input type="text" placeholder='Subject' className='bg-white rounded px-2 py-1 w-96' />
-                    <textarea className='bg-white rounded px-2 py-1 w-96 ' placeholder='Your Message' rows={4}></textarea>
+                    <input type="email" placeholder='Email Address' className='bg-white rounded px-4 py-2 w-96 focus:ring-primary focus:ring-2 outline-none ' />
+                    <input type="text" placeholder='Subject' className='bg-white rounded px-4 py-2 w-96 focus:ring-primary focus:ring-2 outline-none' />
+                    <textarea className='bg-white rounded px-4 py-2 w-96 focus:ring-primary focus:ring-2 outline-none' placeholder='Your Message' rows={4}></textarea>
                     <MyBtn>Submit</MyBtn>
                 </form>
             </div>
